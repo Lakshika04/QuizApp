6f600bb6fd0fc2f85b8ab1be54de11d0210ff8ef
Lakshika04 <krishikaa204@gmail.com>
2025-11-04 23:56:24 +0530

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,50 +1,293 @@
-import React from 'react'
+// import React, { useState } from 'react'
+// import { navbarStyles } from '../assets/dummystyle'
+// import { Link, NavLink, useNavigate } from 'react-router-dom'
+// import { Award, LogIn, LogOut, Menu, X } from 'lucide-react';
+
+// const Navbar = (logoSrc) => {
+
+// const navigate= useNavigate();
+// const[loggedIn, setLoggedIn]= useState(false);
+// const[menuOpen,setMenuOpen]=useState(false);
+
+
+// //LOgin funtion
+// const handleLogout=()=>{
+//   try {
+//     localStorage.removeItem('authToken')
+//     localStorage.clear();
+//   }
+//    catch (error) {
+//     //ignore all error
+//   }
+//   window.dispatchEvent(
+//     new CustomEvent('authChanged',{detail:{user: null}})
+//   )
+//   setMenuOpen(false);
+//   try{
+//     navigate('/login');
+//   }
+//   catch(error){
+//     window.location.href="/login";
+//   }
+// }
+
+//   return (
+//     <div>
+//         <nav className={navbarStyles.nav}>
+//             <div
+//                 style={{
+//                     backgroundImage: navbarStyles.decorativePatternBackground,
+//                 }}
+//                 className={navbarStyles.decorativePattern}
+//             ></div>
+
+//             <div className={navbarStyles.bubble1}></div>
+
+//               <div className={navbarStyles.bubble2}></div>
+
+//                 <div className={navbarStyles.bubble3}></div>
+
+//                 <div className={navbarStyles.container}>
+//                    < div className={navbarStyles.logoContainer}>
+
+//                    <Link to='/' className={navbarStyles.logoButton}>
+//                    <div className={navbarStyles.logoInner}>
+//                     <img
+//                 src={
+//                   logoSrc ||
+//                   "https://yt3.googleusercontent.com/eD5QJD-9uS--ekQcA-kDTCu1ZO4d7d7BTKLIVH-EySZtDVw3JZcc-bHHDOMvxys92F7rD8Kgfg=s900-c-k-c0x00ffffff-no-rj"
+//                 }
+//                 alt="QuizMaster logo"
+//                 className={navbarStyles.logoImage}
+//               />
+
+//                    </div>
+//                    </Link>
+
+//                    <div className={navbarStyles.titleContainer}>
+//                     <div className={navbarStyles.titleBackground}>
+//                         <h1 className={navbarStyles.titleText}>Quiz Application</h1>
+//                     </div>
+//                    </div>
+
+//                    <div className={navbarStyles.desktopButtonsContainer}>
+//                     <div className={navbarStyles.spacer}></div>
+
+//                     <NavLink to='/result'className={navbarStyles.resultsButton}>
+//                     <Award className={navbarStyles.buttonIcon}/>
+//                     My result
+
+//                     </NavLink>
+//                     {loggedIn ?(
+//                       <button onClick={handleLogout} className={navbarStyles.logoutButton}>
+//                         <LogOut className={navbarStyles.buttonIcon}/>
+//                         Logout
+
+//                       </button>
+//                     ): (
+//                       <NavLink to='/login' className={navbarStyles.loginButton}>
+//                       <LogIn className={navbarStyles.buttonIcon} />
+//                         login
+//                       </NavLink>
+//                     )}
+//         </div>
+
+//         <div className={navbarStyles.mobileMenuContainer}>
+//                     <button onClick={()=>setMenuOpen((s)=>!s)} className={navbarStyles.menuToggleButton}>
+//                       {menuOpen?(
+//                         <X className={navbarStyles.menuIcon}/>
+//                       ) : (
+//                         <Menu className={navbarStyles.menuIcon}/>
+//                       )}
+
+//                     </button>
+
+//                     {menuOpen && (
+//                       <div className={navbarStyles.mobileMenuPanel}>
+//                         <ul className={navbarStyles.mobileMenuList}>
+//                           <li>
+//                             <NavLink to='/result' className={navbarStyles.mobileMenuItem} onClick={()=>setMenuOpen(false)}>
+//                             <Award className={navbarStyles.mobileMenuIcon}/>
+//                             my result
+
+//                             </NavLink>
+//                           </li>
+
+//                           {loggedIn ?(
+//                             <button type='button ' onClick={handleLogout} className={navbarStyles.mobileMenuItem}>
+//                               <LogOut className={navbarStyles.mobileMenuIcon}/>
+//                               logout
+
+//                             </button>
+
+//                           ): (
+//                             <li>
+//                               <NavLink to='/login' className={navbarStyles.mobileMenuItem} onClick={()=>setMenuOpen(false)}>
+//                               <LogIn className={navbarStyles.mobileMenuIcon}/>
+//                               login
+
+//                               </NavLink>
+//                             </li>
+//                           )}
+
+//                         </ul>
+
+//                       </div>
+//                     )}
+//         </div>
+
+//                 </div>
+//                 </div>
+
+//                     <style >
+//                       {navbarStyles.animations}
+
+//                     </style>
+
+//         </nav>
+
+
+//     </div>
+//   )
+// }
+
+// export default Navbar
+
+import React, { useState } from 'react'
 import { navbarStyles } from '../assets/dummystyle'
-import { Link } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { Award, LogIn, LogOut, Menu, X } from 'lucide-react';
 
 const Navbar = (logoSrc) => {
+
+const navigate= useNavigate();
+const[loggedIn, setLoggedIn]= useState(false);
+const[menuOpen,setMenuOpen]=useState(false);
+
+//LOgin funtion
+const handleLogout=()=>{
+  try {
+    localStorage.removeItem('authToken')
+    localStorage.clear();
+  }
+  catch (error) {
+    //ignore all error
+  }
+  window.dispatchEvent(
+    new CustomEvent('authChanged',{detail:{user: null}})
+  )
+  setMenuOpen(false);
+  try{
+    navigate('/login');
+  }
+  catch(error){
+    window.location.href="/login";
+  }
+}
+
   return (
     <div>
-        <nav className={navbarStyles.nav}>
-            <div
-                style={{
-                    backgroundImage: navbarStyles.decorativePatternBackground,
-                }}
-                className={navbarStyles.decorativePattern}
-            ></div>
+      <nav className={navbarStyles.nav}>
+        <div
+          style={{
+            backgroundImage: navbarStyles.decorativePatternBackground,
+          }}
+          className={navbarStyles.decorativePattern}
+        ></div>
 
-            <div className={navbarStyles.bubble1}></div>
+        <div className={navbarStyles.bubble1}></div>
+        <div className={navbarStyles.bubble2}></div>
+        <div className={navbarStyles.bubble3}></div>
 
-              <div className={navbarStyles.bubble2}></div>
+        <div className={navbarStyles.container}>
+          <div className={navbarStyles.logoContainer}>
 
-                <div className={navbarStyles.bubble3}></div>
+            <Link to='/' className={navbarStyles.logoButton}>
+              <div className={navbarStyles.logoInner}>
+                <img
+                  src={
+                    logoSrc ||
+                    "https://yt3.googleusercontent.com/eD5QJD-9uS--ekQcA-kDTCu1ZO4d7d7BTKLIVH-EySZtDVw3JZcc-bHHDOMvxys92F7rD8Kgfg=s900-c-k-c0x00ffffff-no-rj"
+                  }
+                  alt="QuizMaster logo"
+                  className={navbarStyles.logoImage}
+                />
+              </div>
+            </Link>
 
-                <div className={navbarStyles.container}>
-                   < div className={navbarStyles.logoContainer}>
+            <div className={navbarStyles.titleContainer}>
+              <div className={navbarStyles.titleBackground}>
+                <h1 className={navbarStyles.titleText}>Quiz Application</h1>
+              </div>
+            </div>
 
-                   <Link to='/' className={navbarStyles.logoButton}>
-                   <div className={navbarStyles.logoInner}>
-                    <img
-                src={
-                  logoSrc ||
-                  "https://yt3.googleusercontent.com/eD5QJD-9uS--ekQcA-kDTCu1ZO4d7d7BTKLIVH-EySZtDVw3JZcc-bHHDOMvxys92F7rD8Kgfg=s900-c-k-c0x00ffffff-no-rj"
-                }
-                alt="QuizMaster logo"
-                className={navbarStyles.logoImage}
-              />
+          </div>
 
-                   </div>
-                   </Link>
+          <div className={navbarStyles.desktopButtonsContainer}>
+            <div className={navbarStyles.spacer}></div>
 
-                   <div className={navbarStyles.titleContainer}>
-                    <div className={navbarStyles.titleBackground}>
-                        <h1 className={navbarStyles.titleText}>Quiz Application</h1>
-                    </div>
-                   </div>
+            <NavLink to='/result'className={navbarStyles.resultsButton}>
+              <Award className={navbarStyles.buttonIcon}/>
+              My result
+            </NavLink>
 
-                </div>
-                </div>
-        </nav>
+            {loggedIn ?(
+              <button onClick={handleLogout} className={navbarStyles.logoutButton}>
+                <LogOut className={navbarStyles.buttonIcon}/>
+                Logout
+              </button>
+            ): (
+              <NavLink to='/login' className={navbarStyles.loginButton}>
+                <LogIn className={navbarStyles.buttonIcon} />
+                login
+              </NavLink>
+            )}
+          </div>
+
+          <div className={navbarStyles.mobileMenuContainer}>
+            <button onClick={()=>setMenuOpen((s)=>!s)} className={navbarStyles.menuToggleButton}>
+              {menuOpen?(
+                <X className={navbarStyles.menuIcon}/>
+              ) : (
+                <Menu className={navbarStyles.menuIcon}/>
+              )}
+            </button>
+
+            {menuOpen && (
+              <div className={navbarStyles.mobileMenuPanel}>
+                <ul className={navbarStyles.mobileMenuList}>
+                  <li>
+                    <NavLink to='/result' className={navbarStyles.mobileMenuItem} onClick={()=>setMenuOpen(false)}>
+                      <Award className={navbarStyles.mobileMenuIcon}/>
+                      my result
+                    </NavLink>
+                  </li>
+
+                  {loggedIn ?(
+                    <button type='button ' onClick={handleLogout} className={navbarStyles.mobileMenuItem}>
+                      <LogOut className={navbarStyles.mobileMenuIcon}/>
+                      logout
+                    </button>
+                  ): (
+                    <li>
+                      <NavLink to='/login' className={navbarStyles.mobileMenuItem} onClick={()=>setMenuOpen(false)}>
+                        <LogIn className={navbarStyles.mobileMenuIcon}/>
+                        login
+                      </NavLink>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )}
+          </div>
+
+        </div>
+      </nav>
+
+      <style >
+        {navbarStyles.animations}
+      </style>
+
     </div>
   )
 }
