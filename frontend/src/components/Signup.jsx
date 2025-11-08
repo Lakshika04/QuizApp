ec3bc0be1dc298576f4b90af2bd4deacf5d4d8a4
Lakshika04 <krishikaa204@gmail.com>
2025-11-08 16:59:36 +0530

    complete signup

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { signupStyles } from '../assets/dummystyle'
 import { Link, useNavigate } from 'react-router-dom';
-import { ArrowLeft, CheckCircle, User } from 'lucide-react';
+import { ArrowLeft, CheckCircle, Eye, EyeOff, Lock, Mail, User } from 'lucide-react';
 
 
 const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);  //whether email has @ or not
@@ -144,16 +144,135 @@ const Signup = ({ onSignupSuccess = null }) => {
                         </span>
 
                         <input type="text" name='name' value={name} onChange={(e)=>{
-                            setName(e.target.value),
-                            setErrors((s) =>({     ...s, 
-                                    name: undefined})
+                            setName(e.target.value);
+                            if(errors.name)
+                                   setErrors((s) =>({    
+                                   ...s, 
+                                    name: undefined,
+                                  })
                             );
-                        }}/>
+                            
+                       
+                        }}
+                        className={`${signupStyles.input} ${
+                          errors.name
+                          ? signupStyles.inputError
+                          : signupStyles.inputNormal
+                        }`}
+                        placeholder='john Doe'
+                        required
+                        />
                     </div>
+                    {errors.name && (
+                      <p className={signupStyles.errorText}>
+                        {errors.name}
+                      </p>
+                    )}
                 </label>
+
+                      <label className={signupStyles.label}>
+                    <span className={signupStyles.labelText}> email</span>
+                    <div className={signupStyles.inputContainer}>
+                        <span className={signupStyles.inputIcon}>
+                            <Mail className={signupStyles.inputContainer}/>
+                        </span>
+
+                        <input type="email" name='email' value={email} onChange={(e)=>{
+                            setEmail(e.target.value);
+                            if(errors.email)
+                                   setErrors((s) =>({    
+                                   ...s, 
+                                    email: undefined,
+                                  })
+                            );
+                            
+                       
+                        }}
+                        className={`${signupStyles.input} ${
+                          errors.email
+                          ? signupStyles.inputError
+                          : signupStyles.inputNormal
+                        }`}
+                        placeholder='your@example.com'
+                        required
+                        />
+                    </div>
+                    {errors.email && (
+                      <p className={signupStyles.errorText}>
+                        {errors.email}
+                      </p>
+                    )}
+                </label>
+
+                  <label className={signupStyles.label}>
+                    <span className={signupStyles.labelText}> password</span>
+                    <div className={signupStyles.inputContainer}>
+                        <span className={signupStyles.inputIcon}>
+                            <Lock className={signupStyles.inputContainer}/>
+                        </span>
+
+                        <input type={showPassword? 'text': 'password'} name='password' value={password} onChange={(e)=>{
+                            setPassword(e.target.value);
+                            if(errors.password)
+                                   setErrors((s) =>({    
+                                   ...s, 
+                                    password: undefined,
+                                  })
+                            );
+                            
+                       
+                        }}
+                        className={`${signupStyles.input} ${signupStyles.passwordInput} ${
+                          errors.password
+                          ? signupStyles.inputError
+                          : signupStyles.inputNormal
+                        }`}
+                        placeholder='create a password'
+                        required
+                        />
+                            {/* Toggle Password Visibility Button */}
+                    <button type="button" className={signupStyles.passwordToggle} onClick={() => setShowPassword((prev) => !prev)}>
+                      {showPassword ? (
+                        <EyeOff className={signupStyles.passwordToggleIcon} />
+                      ) : (
+                        <Eye className={signupStyles.passwordToggleIcon} />
+                      )}
+                    </button>
+
+                    </div>
+                    {errors.password && (
+                      <p className={signupStyles.errorText}>
+                        {errors.password}
+                      </p>
+                    )}
+                </label>
+
+                 {submitError && (
+                    <p className={signupStyles.submitError} role='alert'>{submitError}</p>
+                    )}
+
+                    <div className={signupStyles.buttonsContainer}>
+                      <button type='submit' disabled={loading} className={signupStyles.submitButton}>
+                        {loading? 'creating account....' : 'create account'}
+
+                      </button>
+                    </div>
+
             </div>
         </div>
         </form>
+
+        <div className={signupStyles.loginPromptContainer}>
+          <div className={signupStyles.loginPromptContent}>
+          <span className={signupStyles.loginPromptText}>
+            already have an account?
+          </span>
+          <Link to='/login' className={signupStyles.loginPromptLink}>
+          login
+          </Link>
+          </div>
+
+        </div>
       </div>
 
     </div>
